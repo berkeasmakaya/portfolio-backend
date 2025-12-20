6a0eef180cbabdc316a37f0313c635ac57ae9fff
Berke <asmakayab@gmail.com>
2025-12-20 21:02:24 +0300

    feat: add API endpoints for projects and skills with viewsets and serializers

--- skills/views.py
+++ skills/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
+from .models import Skill
+from .serializers import SkillSerializer
 
-# Create your views here.
+class SkillViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Skill.objects.all()
+    serializer_class = SkillSerializer
+    permission_classes = [AllowAny]
